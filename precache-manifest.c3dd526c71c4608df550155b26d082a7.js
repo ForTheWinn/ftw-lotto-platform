ea14e5e72a8e08b73beb7906978eb4572f365a84
ED <eddie@ftwcoin.io>
2020-08-27 23:09:37 -0700

    Updates

---
+++ precache-manifest.c3dd526c71c4608df550155b26d082a7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b76e491fdc363fe47d3ec153266d10",
+    "url": "/ftw-lotto-platform/index.html"
+  },
+  {
+    "revision": "34e9ba50fab936601e53",
+    "url": "/ftw-lotto-platform/static/css/14.57904731.chunk.css"
+  },
+  {
+    "revision": "2d9f98a502b943de2dd0",
+    "url": "/ftw-lotto-platform/static/css/main.df31754f.chunk.css"
+  },
+  {
+    "revision": "f6132b02395ece2cb622",
+    "url": "/ftw-lotto-platform/static/js/0.860dee62.chunk.js"
+  },
+  {
+    "revision": "17536e8aa295fc059c08",
+    "url": "/ftw-lotto-platform/static/js/1.c850f98e.chunk.js"
+  },
+  {
+    "revision": "8c8a345735e14778ca6e",
+    "url": "/ftw-lotto-platform/static/js/10.da615b9a.chunk.js"
+  },
+  {
+    "revision": "305e8c47a26e6262e248",
+    "url": "/ftw-lotto-platform/static/js/11.7854591f.chunk.js"
+  },
+  {
+    "revision": "34e9ba50fab936601e53",
+    "url": "/ftw-lotto-platform/static/js/14.1be31739.chunk.js"
+  },
+  {
+    "revision": "5f48e988dc7d676b8a77f8cb2efe1504",
+    "url": "/ftw-lotto-platform/static/js/14.1be31739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18aad29d8fdc8daad6d0",
+    "url": "/ftw-lotto-platform/static/js/2.0e7c03bb.chunk.js"
+  },
+  {
+    "revision": "a59eaa359b94a5b49d2e",
+    "url": "/ftw-lotto-platform/static/js/3.1f038d63.chunk.js"
+  },
+  {
+    "revision": "bc887978648562561c59",
+    "url": "/ftw-lotto-platform/static/js/4.22e4c5ab.chunk.js"
+  },
+  {
+    "revision": "bf56c1d0d2b726814400",
+    "url": "/ftw-lotto-platform/static/js/5.f38c5c10.chunk.js"
+  },
+  {
+    "revision": "d6a2ba86c22245fd2247",
+    "url": "/ftw-lotto-platform/static/js/6.f1b1e182.chunk.js"
+  },
+  {
+    "revision": "ac6f554c5def130fcfcc",
+    "url": "/ftw-lotto-platform/static/js/7.0cfbb268.chunk.js"
+  },
+  {
+    "revision": "24e0043cbead99ccc412",
+    "url": "/ftw-lotto-platform/static/js/8.c7d0286e.chunk.js"
+  },
+  {
+    "revision": "880008a2054eaef19704",
+    "url": "/ftw-lotto-platform/static/js/9.bce16ad3.chunk.js"
+  },
+  {
+    "revision": "2d9f98a502b943de2dd0",
+    "url": "/ftw-lotto-platform/static/js/main.1d0dd8f5.chunk.js"
+  },
+  {
+    "revision": "dc1a8362835c9f1f399b",
+    "url": "/ftw-lotto-platform/static/js/runtime-main.7800ab22.js"
+  }
+]);
